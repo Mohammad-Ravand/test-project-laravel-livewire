27163905e00b4d49ae71f927a179cc45e859328e
Mohammad <mohammadwebdeveloperiran@gmail.com>
2025-10-27 20:39:52 +0330

    fix(post-item): update delete button icon and improve spacing in header

--- resources/views/components/posts/post-item.blade.php
+++ resources/views/components/posts/post-item.blade.php
@@ -7,14 +7,16 @@
         <!-- Header with icons -->
         <div class="flex justify-between items-start mb-4">
             <h3 class="text-xl font-bold leading-tight">{{ $post->title }}</h3>
-            <div class="flex space-x-2 space-x-reverse">
+            <div class="flex gap-x-2 space-x-2 space-x-reverse">
                 <button wire:click="delete({{ $post->id }})"
                     class="p-1.5 rounded-lg bg-white/20 hover:bg-white/30 transition-colors">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9zM4 5a2 2 0 012-2h8a2 2 0 012 2v6a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 102 0v3a1 1 0 11-2 0V9zm4 0a1 1 0 10-2 0v3a1 1 0 102 0V9z"
-                            clip-rule="evenodd"></path>
+
+                    <svg w-5 h-5 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
+
                 </button>
                 <button wire:click="edit({{ $post->id }})"
                     class="p-1.5 rounded-lg bg-white/20 hover:bg-white/30 transition-colors">
